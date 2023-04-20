ae99bce25818def4319b7dc1d1f589ad1cfff63a
Aamir Muhammad <aamir.muhammad@tib.eu>
2023-04-20 12:22:35 +0200

    Touched and Dirty States

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -1555,7 +1555,537 @@
 //   );
 // };
 
-// lecture 18. Get Field Values
+// // lecture 18. Get Field Values
+// import { useForm, useFieldArray } from 'react-hook-form';
+// import { DevTool } from '@hookform/devtools';
+// import { useEffect } from 'react';
+
+// let renderCount = 0;
+// type FormValues = {
+//   username: String;
+//   email: String;
+//   channel: String;
+//   social: {
+//     twitter: String;
+//     facebook: String;
+//   };
+//   phoneNumbers: String[];
+//   phNumbers: { number: String }[];
+//   age: number;
+//   dob: Date;
+// };
+
+// export const YouTubeForm = () => {
+//   const form = useForm<FormValues>({
+//     defaultValues: async () => {
+//       const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+//       const data = await response.json();
+//       return {
+//         username: 'aamir',
+//         email: data.email,
+//         channel: 'aamir',
+//         social: {
+//           twitter: '',
+//           facebook: '',
+//         },
+//         phoneNumbers: ['', ''],
+//         phNumbers: [{ number: '' }],
+//         age: 0,
+//         dob: new Date(),
+//       };
+//     },
+//   });
+//   console.log(form);
+//   const { register, control, handleSubmit, formState, watch, getValues } = form;
+//   const { errors } = formState;
+
+//   const { fields, append, remove } = useFieldArray({
+//     name: 'phNumbers',
+//     control,
+//   });
+
+//   // const watchUsername = watch('username');
+//   // const watchUsernameEmail = watch(['username', 'email']);
+//   // const watchForm = watch();
+
+//   useEffect(() => {
+//     const subscription = watch((value) => {
+//       // the value here will be the updated version of the form fields
+//       console.log(value);
+//     });
+//     return () => subscription.unsubscribe();
+//   }, [watch]);
+
+//   // const handleGetValues = () => {
+//   //   console.log('Get Field Values', getValues());
+//   // };
+
+//   // const handleGetValues = () => {
+//   //   console.log('Get Field Values', getValues('social'));
+//   // };
+
+//   const handleGetValues = () => {
+//     console.log('Get Field Values', getValues(['username', 'email']));
+//   };
+
+//   renderCount++;
+//   const onSubmit = (data: FormValues) => {
+//     console.log('Form submitted', data);
+//   };
+//   return (
+//     <div>
+//       <h1>YouTube Form (renderCount: {renderCount / 2})</h1>
+//       {/* <h2>Watched value: {watchUsername}</h2>
+//       <h2>Watched value: {watchUsernameEmail}</h2> */}
+//       {/* <h2>Watched value: {JSON.stringify(watchForm)}</h2> */}
+
+//       <form onSubmit={handleSubmit(onSubmit)} noValidate>
+//         <div className="form-control">
+//           <label htmlFor="username">Username</label>
+//           <input
+//             type="text"
+//             id="username"
+//             {...register('username', {
+//               required: {
+//                 value: true,
+//                 message: 'Username is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.username?.message}</p>
+//         </div>
+//         <div className="form-control">
+//           <label htmlFor="email">E-mail</label>
+//           <input
+//             type="email"
+//             id="email"
+//             {...register('email', {
+//               pattern: {
+//                 value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*$/,
+//                 message: 'Invalid email format',
+//               },
+//               validate: {
+//                 notAdmin: (fieldValue) => {
+//                   return fieldValue !== 'admin@example.com' || 'Enter a different email address';
+//                 },
+//                 notBlackListed: (fieldValue) => {
+//                   return !fieldValue.endsWith('baddomain.com') || 'This domain is not supported';
+//                 },
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.email?.message}</p>
+//         </div>
+//         <div className="form-control">
+//           <label htmlFor="channel">Channel</label>
+//           <input
+//             type="text"
+//             id="channel"
+//             {...register('channel', {
+//               required: {
+//                 value: true,
+//                 message: 'channel is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.channel?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="twitter">Twitter</label>
+//           <input
+//             type="text"
+//             id="twitter"
+//             {...register('social.twitter', {
+//               required: {
+//                 value: true,
+//                 message: 'twitter account is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.social?.twitter?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="facebook">Facebook</label>
+//           <input
+//             type="text"
+//             id="facebook"
+//             {...register('social.facebook', {
+//               required: {
+//                 value: true,
+//                 message: 'facebook account is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.social?.facebook?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="primary-phone">Primary phone number</label>
+//           <input
+//             type="text"
+//             id="primary-phone"
+//             {...register('phoneNumbers.0', {
+//               required: {
+//                 value: true,
+//                 message: 'Primary Number is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.phoneNumbers?.[0]?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="secondary-phone">Secondary phone number</label>
+//           <input
+//             type="text"
+//             id="secondary-phone"
+//             {...register('phoneNumbers.1', {
+//               required: {
+//                 value: true,
+//                 message: 'Secondary Number is required',
+//               },
+//             })}
+//           />
+//         </div>
+//         <p className="error">{errors.phoneNumbers?.[1]?.message}</p>
+
+//         <div>
+//           <label>List of phone numbers</label>
+//           <div>
+//             {fields.map((field, index) => {
+//               return (
+//                 <div className="form-control" key={field.id}>
+//                   <input type="text" {...register(`phNumbers.${index}.number` as const)} />
+//                   {index > 0 && (
+//                     <button type="button" onClick={() => remove(index)}>
+//                       Remove
+//                     </button>
+//                   )}
+//                 </div>
+//               );
+//             })}
+//             <button type="button" onClick={() => append({ number: '' })}>
+//               Add phone number
+//             </button>
+//           </div>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="age">Age</label>
+//           <input
+//             type="number"
+//             id="age"
+//             {...register('age', {
+//               valueAsNumber: true,
+//               required: {
+//                 value: true,
+//                 message: 'age is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.age?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="dob">Date of Birth</label>
+//           <input
+//             type="date"
+//             id="dob"
+//             {...register('dob', {
+//               valueAsDate: true,
+//               required: {
+//                 value: true,
+//                 message: 'Date of Birth is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.dob?.message}</p>
+//         </div>
+//         <button type="button" onClick={() => handleGetValues()}>
+//           Get Field Values
+//         </button>
+
+//         <button>Submit</button>
+//       </form>
+//       <DevTool control={control} />
+//     </div>
+//   );
+// };
+
+// // lecture 18. Set Field Values
+// import { useForm, useFieldArray } from 'react-hook-form';
+// import { DevTool } from '@hookform/devtools';
+// import { useEffect } from 'react';
+
+// let renderCount = 0;
+// type FormValues = {
+//   username: String;
+//   email: String;
+//   channel: String;
+//   social: {
+//     twitter: String;
+//     facebook: String;
+//   };
+//   phoneNumbers: String[];
+//   phNumbers: { number: String }[];
+//   age: number;
+//   dob: Date;
+// };
+
+// export const YouTubeForm = () => {
+//   const form = useForm<FormValues>({
+//     defaultValues: async () => {
+//       const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+//       const data = await response.json();
+//       return {
+//         username: 'aamir',
+//         email: data.email,
+//         channel: 'aamir',
+//         social: {
+//           twitter: '',
+//           facebook: '',
+//         },
+//         phoneNumbers: ['', ''],
+//         phNumbers: [{ number: '' }],
+//         age: 0,
+//         dob: new Date(),
+//       };
+//     },
+//   });
+//   console.log(form);
+//   const { register, control, handleSubmit, formState, watch, getValues, setValue } = form;
+//   const { errors } = formState;
+
+//   const { fields, append, remove } = useFieldArray({
+//     name: 'phNumbers',
+//     control,
+//   });
+
+//   // const watchUsername = watch('username');
+//   // const watchUsernameEmail = watch(['username', 'email']);
+//   // const watchForm = watch();
+
+//   useEffect(() => {
+//     const subscription = watch((value) => {
+//       // the value here will be the updated version of the form fields
+//       console.log(value);
+//     });
+//     return () => subscription.unsubscribe();
+//   }, [watch]);
+
+//   // const handleGetValues = () => {
+//   //   console.log('Get Field Values', getValues());
+//   // };
+
+//   // const handleGetValues = () => {
+//   //   console.log('Get Field Values', getValues('social'));
+//   // };
+
+//   const handleGetValues = () => {
+//     console.log('Get Field Values', getValues(['username', 'email']));
+//   };
+
+//   const handleSetValues = () => {
+//     setValue('username', '', {
+//       shouldDirty: true,
+//       shouldTouch: true,
+//       shouldValidate: true,
+//     });
+//   };
+
+//   renderCount++;
+//   const onSubmit = (data: FormValues) => {
+//     console.log('Form submitted', data);
+//   };
+//   return (
+//     <div>
+//       <h1>YouTube Form (renderCount: {renderCount / 2})</h1>
+//       {/* <h2>Watched value: {watchUsername}</h2>
+//       <h2>Watched value: {watchUsernameEmail}</h2> */}
+//       {/* <h2>Watched value: {JSON.stringify(watchForm)}</h2> */}
+
+//       <form onSubmit={handleSubmit(onSubmit)} noValidate>
+//         <div className="form-control">
+//           <label htmlFor="username">Username</label>
+//           <input
+//             type="text"
+//             id="username"
+//             {...register('username', {
+//               required: {
+//                 value: true,
+//                 message: 'Username is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.username?.message}</p>
+//         </div>
+//         <div className="form-control">
+//           <label htmlFor="email">E-mail</label>
+//           <input
+//             type="email"
+//             id="email"
+//             {...register('email', {
+//               pattern: {
+//                 value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*$/,
+//                 message: 'Invalid email format',
+//               },
+//               validate: {
+//                 notAdmin: (fieldValue) => {
+//                   return fieldValue !== 'admin@example.com' || 'Enter a different email address';
+//                 },
+//                 notBlackListed: (fieldValue) => {
+//                   return !fieldValue.endsWith('baddomain.com') || 'This domain is not supported';
+//                 },
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.email?.message}</p>
+//         </div>
+//         <div className="form-control">
+//           <label htmlFor="channel">Channel</label>
+//           <input
+//             type="text"
+//             id="channel"
+//             {...register('channel', {
+//               required: {
+//                 value: true,
+//                 message: 'channel is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.channel?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="twitter">Twitter</label>
+//           <input
+//             type="text"
+//             id="twitter"
+//             {...register('social.twitter', {
+//               required: {
+//                 value: true,
+//                 message: 'twitter account is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.social?.twitter?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="facebook">Facebook</label>
+//           <input
+//             type="text"
+//             id="facebook"
+//             {...register('social.facebook', {
+//               required: {
+//                 value: true,
+//                 message: 'facebook account is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.social?.facebook?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="primary-phone">Primary phone number</label>
+//           <input
+//             type="text"
+//             id="primary-phone"
+//             {...register('phoneNumbers.0', {
+//               required: {
+//                 value: true,
+//                 message: 'Primary Number is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.phoneNumbers?.[0]?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="secondary-phone">Secondary phone number</label>
+//           <input
+//             type="text"
+//             id="secondary-phone"
+//             {...register('phoneNumbers.1', {
+//               required: {
+//                 value: true,
+//                 message: 'Secondary Number is required',
+//               },
+//             })}
+//           />
+//         </div>
+//         <p className="error">{errors.phoneNumbers?.[1]?.message}</p>
+
+//         <div>
+//           <label>List of phone numbers</label>
+//           <div>
+//             {fields.map((field, index) => {
+//               return (
+//                 <div className="form-control" key={field.id}>
+//                   <input type="text" {...register(`phNumbers.${index}.number` as const)} />
+//                   {index > 0 && (
+//                     <button type="button" onClick={() => remove(index)}>
+//                       Remove
+//                     </button>
+//                   )}
+//                 </div>
+//               );
+//             })}
+//             <button type="button" onClick={() => append({ number: '' })}>
+//               Add phone number
+//             </button>
+//           </div>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="age">Age</label>
+//           <input
+//             type="number"
+//             id="age"
+//             {...register('age', {
+//               valueAsNumber: true,
+//               required: {
+//                 value: true,
+//                 message: 'age is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.age?.message}</p>
+//         </div>
+
+//         <div className="form-control">
+//           <label htmlFor="dob">Date of Birth</label>
+//           <input
+//             type="date"
+//             id="dob"
+//             {...register('dob', {
+//               valueAsDate: true,
+//               required: {
+//                 value: true,
+//                 message: 'Date of Birth is required',
+//               },
+//             })}
+//           />
+//           <p className="error">{errors.dob?.message}</p>
+//         </div>
+//         <button type="button" onClick={handleGetValues}>
+//           Get Field Values
+//         </button>
+
+//         <button type="button" onClick={handleSetValues}>
+//           Set Field Values
+//         </button>
+
+//         <button>Submit</button>
+//       </form>
+//       <DevTool control={control} />
+//     </div>
+//   );
+// };
+
+// lecture 20. Touched and Dirty States
 import { useForm, useFieldArray } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 import { useEffect } from 'react';
@@ -1596,8 +2126,12 @@ export const YouTubeForm = () => {
     },
   });
   console.log(form);
-  const { register, control, handleSubmit, formState, watch, getValues } = form;
-  const { errors } = formState;
+  const { register, control, handleSubmit, formState, watch, getValues, setValue } = form;
+  const { errors, dirtyFields, touchedFields, isDirty } = formState;
+
+  console.log('dirtyFields ---', dirtyFields);
+  console.log('touchedFields ---', touchedFields);
+  console.log('isDirty ---', isDirty);
 
   const { fields, append, remove } = useFieldArray({
     name: 'phNumbers',
@@ -1626,6 +2160,14 @@ export const YouTubeForm = () => {
 
   const handleGetValues = () => {
     console.log('Get Field Values', getValues(['username', 'email']));
+  };
+
+  const handleSetValues = () => {
+    setValue('username', '', {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
   };
 
   renderCount++;
@@ -1803,8 +2345,12 @@ export const YouTubeForm = () => {
           />
           <p className="error">{errors.dob?.message}</p>
         </div>
-        <button type="button" onClick={() => handleGetValues()}>
+        <button type="button" onClick={handleGetValues}>
           Get Field Values
+        </button>
+
+        <button type="button" onClick={handleSetValues}>
+          Set Field Values
         </button>
 
         <button>Submit</button>
